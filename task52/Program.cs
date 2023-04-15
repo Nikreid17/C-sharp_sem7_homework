3318661141c4dee87a04db23a177da1eed6c6683
Nickolay <nik5te@yandex.ru>
2023-04-15 17:56:12 +0300

    среднее арифметическое в каждом столбце

---
+++ task52/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+Console.Clear();
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+            System.Console.Write($"{matrix[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+System.Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+
+void GetAverageColumn(int[,] matrix)
+{
+    int rows = size[0];
+    int columns = size[1];
+    double[] average = new double[columns];
+    for (int j = 0; j < columns; j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            sum += matrix[i, j];
+        }
+        average[j] = sum / rows;
+    }
+    Console.WriteLine($"Среднее арифметическое: {average}");
+}
+GetAverageColumn(matrix);

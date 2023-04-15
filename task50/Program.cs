aa2c8cdbd9d644de0118b074c60fc303f5a3de7e
Nickolay <nik5te@yandex.ru>
2023-04-15 17:55:33 +0300

    значение элемента по позиции

---
+++ task50/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+
+Console.Clear();
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+            System.Console.Write($"{matrix[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+
+System.Console.Write("Введите строку элемента: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите столбец элемента: ");
+int column = Convert.ToInt32(Console.ReadLine());
+ReleaseArray(matrix);
+
+void ReleaseArray(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i == row && j == column)
+        {
+            System.Console.WriteLine(matrix[row, column]);
+            return;
+        }
+        if (row >= size[0] || column >= size[1])
+        {
+            System.Console.WriteLine("Такого элемента нет.");
+            return;
+        }
+
+    }
+}
+}
